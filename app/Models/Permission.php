8ae35e28041c0c8018bb8d4a442ddf628295a658
gahyun <gahyunlee1119@gmail.com>
2021-12-19 18:59:23 +0900

    db-config

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -3,6 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Permission extends Model {
+    use SoftDeletes;
+    protected $table = 'tb_permission';
+    protected $guarded = [];
 }
